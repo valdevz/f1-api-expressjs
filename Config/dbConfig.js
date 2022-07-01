ba0171344987b19e980dea91b676173cefff8239
Jorge V <jjvaldezr01@gmail.com>
2022-07-01 15:25:46 -0500

    added 404 manage, uri for the project and 404 template

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 function connectDb(dbUrl) {
 
-  console.log('se conecta a: ', dbUrl)
   mongoose.connect(dbUrl, { useUnifiedTopology: true})
     .then(result => {
       console.log('MongoDB connected..........', );
--- Routes/index.js
+++ Routes/index.js
@@ -3,7 +3,10 @@ const express = require('express'),
       RegisterRoutes = require('./register'),
       LoginRoutes = require('./login'),
       Drivers = require('./drivers');
-      Constructors = require('./constructors');
+      Constructors = require('./constructors'),
+      swaggerJsdoc = require("swagger-jsdoc"),
+      { options } = require('../Lib/swaggerDoc'),
+      swaggerUi = require("swagger-ui-express");
 
 app.use('/register', RegisterRoutes)
 
@@ -12,5 +15,8 @@ app.use('/login', LoginRoutes)
 app.use('/drivers', Drivers)
 
 app.use('/constructors', Constructors)
+
+const specs = swaggerJsdoc(options);
+app.use('/',swaggerUi.serve, swaggerUi.setup(specs));
 
 module.exports = app;
